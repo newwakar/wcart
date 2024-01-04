14231cce416d5af21a3b0e788cac5bdf714b59d6
newwakar <127464384+newwakar@users.noreply.github.com>
2024-01-04 22:57:23 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -78,7 +78,7 @@ export const ProtectedRoute = ({children}) => {
 const ProtectedRouteForAdmin = ({children})=> {
   const admin = JSON.parse(localStorage.getItem('user'))
   
-  if(admin.user.email != 'thewakar1@gmail.com'){
+  if(admin.user.email == 'thewakar1@gmail.com'){
     return children
   }
   else{
